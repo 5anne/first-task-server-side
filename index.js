becf2ff897c546be93fd1f3f3d3bb68d33bd44dd
Your Name <you@example.com>
2025-02-05 00:25:51 +0600

    'get_error_solved'

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const allStudentsCollection = client.db('unicresult-Database').collection('student-info');
 
